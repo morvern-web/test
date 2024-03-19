3a7ebe4615f15ae488d7b179dac3919659d94808
jeremy.harris <jeremy.harris@M-JHarris.local>
2024-03-19 15:07:12 +0100

    Updates

--- assets/2024-B_kqDYgK.js
+++
@@ -1,1 +0,0 @@
-const a=[{date:"2024-06-21",venue:"Klub Gromka",location:"Ljubljana, Slovenia",info:'"Happy Hour Bungalow Drip" E.P. Release Party'}];export{a as default};
---
+++ assets/2024-Ca3skh0u.js
@@ -0,0 +1,1 @@
+const a=[{date:"2024-06-21",venue:"Klub Gromka",location:"Ljubljana, Slovenia",info:'w/ Long Dumb Voices - "Happy Hour Bungalow Drip" E.P. Release Party'}];export{a as default};
